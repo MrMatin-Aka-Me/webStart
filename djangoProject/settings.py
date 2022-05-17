c8532f24477956d521637930120572314503ce2c
Matin Juraev <matindzhuraev@gmail.com>
2022-05-17 14:02:48 +0300

    added favicon

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -15,9 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static_files'),
-]
 
 
 # Quick-start development settings - unsuitable for production
@@ -148,7 +145,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static_files/'
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
